490e68d4a4c0ffc01cb332404b5bfbdef23f7fd6
fabien townsend <fabientownsend@gmail.com>
2017-04-18 12:30:33 +0100

    Add test which check quality is never negative
    This test check that the quality of a hand of ragnaros is never negartive even
    when the date is passed

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -2,14 +2,23 @@ defmodule GildedRoseTest do
   use ExUnit.Case
 
   test "hand of ragnaros quality degrades by 1 for one day passed" do
-    item = [%Item{name: "Hand of Ragnaros", sell_in: 4, quality: 3}]
-    result = [%Item{name: "Hand of Ragnaros", sell_in: 3, quality: 2}]
-    assert GildedRose.update_quality(item) == result
+    hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: 4, quality: 3}]
+    updated_hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: 3, quality: 2}]
+
+    assert GildedRose.update_quality(hand_of_ragnaros) == updated_hand_of_ragnaros
   end
 
   test "once the sell by date has passed, hand of ragnaros quality degrades twice as fast" do
-    item = [%Item{name: "Hand of Ragnaros", sell_in: 0, quality: 6}]
-    result = [%Item{name: "Hand of Ragnaros", sell_in: -1, quality: 4}]
-    assert GildedRose.update_quality(item) == result
+    passed_hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: 0, quality: 6}]
+    updated_hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: -1, quality: 4}]
+
+    assert GildedRose.update_quality(passed_hand_of_ragnaros) == updated_hand_of_ragnaros
+  end
+
+  test "the quality of an hand of ragnaros is never negative" do
+    hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: 0, quality: 0}]
+    updated_hand_of_ragnaros = [%Item{name: "Hand of Ragnaros", sell_in: -1, quality: 0}]
+
+    assert GildedRose.update_quality(hand_of_ragnaros) == updated_hand_of_ragnaros
   end
 end
