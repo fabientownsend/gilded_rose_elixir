3b98dd651f9817981e15247b2c04cf33a213af77
fabien townsend <fabientownsend@gmail.com>
2017-04-18 12:19:53 +0100

    Add tests for hand of ragnaros item
    the quality of hand of ragnaros decrease by one each day and decrease
    twice thise value when the date has passed

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,9 +1,15 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
-    item = [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
-    result = [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 7, quality: 5}]
+  test "hand of ragnaros quality degrades by 1 for one day passed" do
+    item = [%Item{name: "Hand of Ragnaros", sell_in: 4, quality: 3}]
+    result = [%Item{name: "Hand of Ragnaros", sell_in: 3, quality: 2}]
+    assert GildedRose.update_quality(item) == result
+  end
+
+  test "once the sell by date has passed, hand of ragnaros quality degrades twice as fast" do
+    item = [%Item{name: "Hand of Ragnaros", sell_in: 0, quality: 6}]
+    result = [%Item{name: "Hand of Ragnaros", sell_in: -1, quality: 4}]
     assert GildedRose.update_quality(item) == result
   end
 end
