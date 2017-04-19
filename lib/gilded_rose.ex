3cc26e09955f4d8247099a7bdf3d88e726cf7b8a
fabien townsend <fabientownsend@gmail.com>
2017-04-19 10:47:06 +0100

    Remove nested verification

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -9,9 +9,10 @@ defmodule GildedRose do
 
   def update_item(item) do
     item = decrease_sell_in(item)
+
     cond do
       is_legendary?(item) -> item
-      is_backstage_passe?(item) -> increase_quality_backstage(item)
+      is_backstage_passe?(item) -> update_backstage(item)
       item.name == "Aged Brie" -> increase_quality(item)
       true ->
         if item.quality > 0 do
@@ -31,30 +32,19 @@ defmodule GildedRose do
   def decrease_quality(item), do: %{item | quality: item.quality - 1}
   def drop_quality_to_zerro(item), do: %{item | quality: 0}
 
-  def increase_quality(item, value) do
-    cond do
-      item.quality < 50 ->
-        %{item | quality: item.quality + value}
-      true -> item
-    end
-  end
-
-  def increase_quality(item) do
-    cond do
-      item.quality < 50 ->
-        increase_quality(item, 1)
-      true -> item
-    end
-  end
-
-  def increase_quality_backstage(item) do
+  def update_backstage(item) do
     cond do
       item.sell_in < 0 -> drop_quality_to_zerro(item)
-      item.sell_in < 5 ->
-        increase_quality(item, 3)
-      item.sell_in < 11 ->
-        increase_quality(item, 2)
-      true -> increase_quality(item, 2)
+      item.sell_in < 5 -> increase_quality(item, 3)
+      item.sell_in < 11 -> increase_quality(item, 2)
+    end
+  end
+
+  def increase_quality(item), do: increase_quality(item, 1)
+  def increase_quality(item, value) do
+    cond do
+      item.quality < 50 -> %{item | quality: item.quality + value}
+      true -> item
     end
   end
 
