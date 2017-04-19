53187b262df3dd32d7b0c998387f9a3582ae757e
fabien townsend <fabientownsend@gmail.com>
2017-04-19 14:56:57 +0100

    Extract to method item redirection

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -7,25 +7,27 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
-  defp update_item(item) do
+  defp update_item(%{name: n} = item) when n == "Sulfuras", do: item
+  defp update_item(item), do: item |> change_sell_in |> change_quality
+
+  defp change_sell_in(item), do: %{item | sell_in: item.sell_in - 1}
+
+  defp change_quality(item) do
     case item.name do
-      "Sulfuras" -> item
-      "Aged Brie" -> item |> update_sell_in |> increase_quality(1)
-      "Hand of Ragnaros" -> item |> update_sell_in |> update_hand_of_ragnaros
-      "Backstage passes to a TAFKAL80ETC concert" -> item |> update_sell_in |> update_backstage
+      "Aged Brie" -> increase_quality(item, 1)
+      "Hand of Ragnaros" -> update_hand_of_ragnaros(item)
+      "Backstage passes to a TAFKAL80ETC concert" -> update_backstage(item)
     end
   end
-
-  defp update_sell_in(item), do: %{item | sell_in: item.sell_in - 1}
-
-  defp update_backstage(%{sell_in: s} = item) when s < 0, do: decrease_quality(item, item.quality)
-  defp update_backstage(%{sell_in: s} = item) when s < 5, do: increase_quality(item, 3)
-  defp update_backstage(%{sell_in: s} = item) when s < 11, do: increase_quality(item, 2)
 
   defp update_hand_of_ragnaros(%{sell_in: s, quality: q} = item) when s < 0 and q > 1 do
     decrease_quality(item, 2)
   end
   defp update_hand_of_ragnaros(item), do: decrease_quality(item, 1)
+
+  defp update_backstage(%{sell_in: s} = item) when s < 0, do: decrease_quality(item, item.quality)
+  defp update_backstage(%{sell_in: s} = item) when s < 5, do: increase_quality(item, 3)
+  defp update_backstage(%{sell_in: s} = item) when s < 11, do: increase_quality(item, 2)
 
   defp decrease_quality(%{quality: q} = item, value) when q > 0, do: quality(item, -value)
   defp decrease_quality(item, _), do: item
